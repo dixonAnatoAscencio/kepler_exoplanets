a8235641e8284f284faafe2ed1e2c4fdb3634e19
dixonAnatoAscencio <dixonjesusanato@gmail.com>
2024-07-22 17:28:42 -0500

    habitable planets

---
+++ index.js
@@ -0,0 +1,34 @@
+const { parse } = require("csv-parse");
+const fs = require("fs");
+
+const results = [];
+
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11
+    && planet['koi_prad'] < 1.6
+  );
+}
+
+fs.createReadStream("kepler_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
+  .on("data", (data) => {
+    if (isHabitablePlanet(data)) {
+      results.push(data);
+    }
+  })
+  .on('error', (err) => {
+    console.error(err);
+  })
+  .on('end', () => {
+    console.log(`${results.length} habitable planets found!`);
+  });
+
+parse();
