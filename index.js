55d68a16ef48641e4bb120a32795b668ee4b804d
dixonAnatoAscencio <dixonjesusanato@gmail.com>
2024-07-22 17:43:54 -0500

    find planets

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const { parse } = require("csv-parse");
 const fs = require("fs");
 
-const results = [];
+const habitablePlanets = [];
 
 function isHabitablePlanet(planet) {
   return (
     planet["koi_disposition"] === "CONFIRMED" &&
     planet["koi_insol"] > 0.36 &&
-    planet["koi_insol"] < 1.11
-    && planet['koi_prad'] < 1.6
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
   );
 }
 
@@ -21,14 +21,19 @@ fs.createReadStream("kepler_data.csv")
   )
   .on("data", (data) => {
     if (isHabitablePlanet(data)) {
-      results.push(data);
+      habitablePlanets.push(data);
     }
   })
-  .on('error', (err) => {
+  .on("error", (err) => {
     console.error(err);
   })
-  .on('end', () => {
-    console.log(`${results.length} habitable planets found!`);
+  .on("end", () => {
+    console.log(
+      habitablePlanets.map((planet) => {
+        return planet["kepler_name"];
+      })
+    );
+    console.log(`${habitablePlanets.length} habitable planets found!`);
   });
 
 parse();
